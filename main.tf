4a95c6424bd6f29373eef9b4032341e32a7f21b7
Martin Sabo <martin.sabo1@ibm.com>
2023-08-29 19:51:49 +0200

    added AKS

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "azurerm_resource_group" "demo" {
 # Define the Virtual Network
 resource "azurerm_virtual_network" "demo" {
   name                = "spoke1"
-  address_space       = ["10.0.0.0/16"]  # Specify your desired address space
+  address_space       = ["10.1.0.0/16"]  # Specify your desired address space
   location            = azurerm_resource_group.demo.location
   resource_group_name = azurerm_resource_group.demo.name
 }
@@ -21,7 +21,7 @@ resource "azurerm_subnet" "backend" {
   name                 = "backend"
   resource_group_name  = azurerm_resource_group.demo.name
   virtual_network_name = azurerm_virtual_network.demo.name
-  address_prefixes     = ["10.0.1.0/24"]  # Specify your desired subnet address range
+  address_prefixes     = ["10.1.0.0/24"]  # Specify your desired subnet address range
 }
 
 # Define the second subnet within the VNet
@@ -29,5 +29,33 @@ resource "azurerm_subnet" "db" {
   name                 = "db"
   resource_group_name  = azurerm_resource_group.demo.name
   virtual_network_name = azurerm_virtual_network.demo.name
-  address_prefixes     = ["10.0.2.0/24"]  # Specify your desired subnet address range
+  address_prefixes     = ["10.1.1.0/24"]  # Specify your desired subnet address range
+}
+
+# Create the Azure Kubernetes Service (AKS) cluster
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "myAKSCluster"
+  location            = azurerm_resource_group.demo.location
+  resource_group_name = azurerm_resource_group.demo.name
+  dns_prefix          = "myaksdns"  # Change to your desired DNS prefix
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2s_v3"  # Change to your desired VM size
+    vnet_subnet_id = azurerm_subnet.backend.id  # Use the existing backend subnet
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin = "azure"
+    load_balancer_sku = "standard"
+  }
+
+  tags = {
+    environment = "dev"
+  }
 }
